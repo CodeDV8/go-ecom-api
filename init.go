568901fa199c84fbf74a26d4165fab614291098a
Thomas Björk <micoxdev@gmail.com>
2020-08-28 18:32:47 +0200

    Minor changes to the auth

--- init.go
+++ init.go
@@ -2,23 +2,38 @@ package main
 
 import (
 	EComApp "github.com/codedv8/go-ecom-app"
-	EComBase "github.com/codedv8/go-ecom-base"
 	EComStructsAPI "github.com/codedv8/go-ecom-structs/API"
 	"github.com/gin-gonic/gin"
 	"log"
 )
 
 func (api *API) SysInit(app *EComApp.Application) {
-
-}
-
-func (api *API) Init(app *EComApp.Application) {
 	// Create apiRouter
 	apiRouter := app.Router.Group("/api/v1")
 	// Create basic auth object
-	basicAuth := EComBase.NewBasicAuth(app, "API")
+	basicAuth := app.UseBasicAuth("API")
 	// Add basic auth to qpiRouter
 	apiRouter.Use(basicAuth.Use())
+	// Add handlers
+	apiRouter.Handle("GET", "/", func(c *gin.Context) {
+		payload := &EComStructsAPI.Root{S: "Hej"}
+		app.CallHook("API_CALL", payload)
+		log.Printf("Returned %+v\n", payload)
+		c.JSON(200, payload)
+	})
+
+	// Add hook
+	app.ListenToHook("API_ADD_ROUTER_HANDLE", func(payload interface{}) (bool, error) {
+		switch v := payload.(type) {
+		case *EComStructsAPI.Root:
+			log.Printf("API_CALL: %+v\n", v)
+			v.S = "Bye bye"
+		}
+		return true, nil
+	})
+}
+
+func (api *API) Init(app *EComApp.Application) {
 	// Add hook
 	app.ListenToHook("API_CALL", func(payload interface{}) (bool, error) {
 		switch v := payload.(type) {
@@ -27,12 +42,5 @@ func (api *API) Init(app *EComApp.Application) {
 			v.S = "Bye bye"
 		}
 		return true, nil
-	})
-	// Add handlers
-	apiRouter.Handle("GET", "/", func(c *gin.Context) {
-		payload := &EComStructsAPI.Root{S: "Hej"}
-		app.CallHook("API_CALL", payload)
-		log.Printf("Returned %+v\n", payload)
-		c.JSON(200, payload)
 	})
 }
